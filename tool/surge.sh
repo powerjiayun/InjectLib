22912994d3807da24bb1b0015ec53f0522af24e1
QiuChenly <QiuChenly@outlook.com>
2023-08-16 11:29:56 +0800

    OPT: 性能优化

--- tool/surge.sh
+++ tool/surge.sh
@@ -14,7 +14,7 @@ else
   echo "非全新安装,跳过清除。"
 fi
 
-echo "感谢QQ 3026***398 用户无偿提供授权信息。"
+echo "感谢QQ 302****398 用户无偿提供授权信息。"
 echo "大胆！检测到你在用盗版软件，这可能会危害你的设备！甚至被国家安全局和保密处就地正法，请三思！"
 
 helper='/Applications/Surge.app/Contents/Library/LaunchServices/com.nssurge.surge-mac.helper'
@@ -23,6 +23,10 @@ echo "正在定位你的Mac物理地址...GPS定位中...你跑不掉了! 即将
 
 echo a5a3: 6A 01 58 C3 |sudo xxd -r - "$helper" #intel
 echo 4172c: 20 00 80 D2 C0 03 5F D6 |sudo xxd -r - "$helper" #arm64
+
+# 版本 5.3.1
+echo 65a3: 6A 01 58 C3 |sudo xxd -r - "$helper" #intel
+echo 972c: 20 00 80 D2 C0 03 5F D6 |sudo xxd -r - "$helper" #arm64
 
 echo "定位你的Mac物理地址完成，正在向国家安全局特工发送你的逮捕许可..."
 offsets=$(grep -a -b -o "\x3C\x73\x74\x72\x69\x6E\x67\x3E\x61\x6E\x63\x68\x6F\x72" $helper | cut -d: -f1)
